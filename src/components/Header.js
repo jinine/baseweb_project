b883fc531e555bc3423112bf47bd688eaa5c63fe
Tristan Engen <tengen@mnbc.ca>
2022-08-10 15:39:35 -0700

    edited the navbar to ADI content

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,17 +14,47 @@ export default function Header() {
   const [mainItems, setMainItems] = React.useState([
     {  
       active: true,
-      icon: Upload, label: "Main A" 
+      icon: Upload, label: "Home" 
     },
     {
       icon: ChevronDown,
-      label: "Main B",
+      label: "About ADI",
       navExitIcon: Delete,
       children: [
-        { icon: Upload, label: "Secondary A"},
-        { icon: Upload, label: "Secondary B" }
+        { icon: Upload, label: "Purpose, Mission, Vision"},
+        { icon: Upload, label: "Who was Amelia Douglas?" }
       ]
-    }
+    },
+    {
+      icon: ChevronDown,
+      label: "Metis Arts & Culture",
+      navExitIcon: Delete,
+      children: [
+        { icon: Upload, label: "Metis Heritage Arts Photo Repository (2D images)"},
+        { icon: Upload, label: "Metis Artists in BC" }
+      ]
+    },
+    {
+      icon: ChevronDown,
+      label: "ADI Exhibitions",
+      navExitIcon: Delete,
+      children: [
+        { icon: Upload, label: "Virtual 3D Exhibitions"},
+        { icon: Upload, label: "Virtual Art Wall Exhibitions" }
+      ]
+    },
+    {
+      icon: ChevronDown,
+      label: "Michif Language",
+      navExitIcon: Delete,
+      children: [
+        { icon: Upload, label: "Information of Michif and Indigenous Language Resources"}
+      ]
+    },
+    {  
+      active: true,
+      icon: Upload, label: "Contact" 
+    },
   ]);
   return (
     <AppNavBar
