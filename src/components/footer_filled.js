0286caacd03768a707667f8abf0d03ae120a373d
jinine <jinine@gmail.com>
2022-08-09 21:18:17 -0700

    cleaned up footer

---
+++ src/components/footer_filled.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Footer from "./footer";
+
+export default function FooterFilled() {
+    
+    return (
+      <Footer>
+        <Footer.Wrapper>
+            <Footer.Title>
+              Thank you for visiting. 
+            </Footer.Title>
+        </Footer.Wrapper>
+      </Footer>
+    );
+  }
