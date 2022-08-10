e35a192cc9caf734bfc85f8df29831c4492ce658
jinine <jinine@gmail.com>
2022-08-09 21:05:00 -0700

    created a basic homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Client as Styletron } from 'styletron-engine-atomic';
 import { Provider as StyletronProvider } from 'styletron-react';
 import { LightTheme, BaseProvider, styled } from 'baseui';
 import Header from './components/Header';
+import Footer from './components/footer';
+// import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
 
 const engine = new Styletron();
 const Centered = styled('div', {
@@ -14,14 +17,26 @@ const Centered = styled('div', {
 
 function App() {
   return (
+    <>
     <StyletronProvider value={engine}>
     <BaseProvider theme={LightTheme}>
     <Header />
       <Centered>
-       
+        <Homepage />
+     
       </Centered>
+      <Footer>
+        <Footer.Wrapper>
+          <Centered>
+            <Footer.Title>
+              Thank you for visiting. 
+            </Footer.Title>
+          </Centered>
+        </Footer.Wrapper>
+      </Footer>
     </BaseProvider>
   </StyletronProvider>
+  </>
   );
 }
 
